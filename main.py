c1f87f62001a132a5677817e73cce4051ea084a7
Sérgio Brito <71470010+Sergio-BR1@users.noreply.github.com>
2022-05-16 02:23:26 +0000

    exclusão de  comentários desnececssários

--- main.py
+++ main.py
@@ -1,37 +1,24 @@
+from math import sqrt
+def F(n):
+    return ((1+sqrt(5))**n-(1-sqrt(5))**n)/(2**n*sqrt(5))
+
+
 memo = {}
-memoC = {}
-#calls = 0
 def fib(X):
-	#global calls
-	#calls+=1
-	#calls[0]+=
-	aux = 0
-	aux +=1
-	if X not in memo:
+	try:
+		return memo[X]
+	except:
 		if X <=1 :
-			#alls[0]+=1
 			memo[X] = X
-			memoC[X] = 1
 			return X
 		else:
-			#calls[0]+=1
 			memo[X] = fib(X-1) + fib(X-2)
-			aux += memoC.get(X-1) + memoC.get(X-2)
-	memoC[X] = aux
-	return memo[X]
+			return memo[X]
 
 N = int(input())
 
 for i in range(N):
 	X = int(input())
-	calls = 0
-	aux = 0
-	'''if (X <= 1):
-		calls = 1
-	else:
-		calls = 2 ** (X-1) '''
 	a = fib(X)
-	print ('fib ('+str(X)+') = ',calls,'calls =', a)
-	print(memoC)
-	#print(memo)
+	print ("fib({0}) = {1:.0f} calls = {2}".format(X, 2*F(X+1)-2, a))
 	
