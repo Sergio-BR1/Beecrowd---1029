0c1cc7468e311febafe075f1fe2fbde64adc92b1
Sérgio Brito <71470010+Sergio-BR1@users.noreply.github.com>
2022-05-16 00:16:19 +0000

    ruim

--- main.py
+++ main.py
@@ -1,25 +1,37 @@
 memo = {}
-def fib(X, calls):
-	calls[0]+=1
-	try:
-		return memo[X]
-	except:
-		#calls[0]+=1
+memoC = {}
+#calls = 0
+def fib(X):
+	#global calls
+	#calls+=1
+	#calls[0]+=
+	aux = 0
+	aux +=1
+	if X not in memo:
 		if X <=1 :
 			#alls[0]+=1
 			memo[X] = X
+			memoC[X] = 1
 			return X
 		else:
 			#calls[0]+=1
-			memo[X] = fib(X-1, calls) + fib(X-2, calls)
-			return memo[X]
+			memo[X] = fib(X-1) + fib(X-2)
+			aux += memoC.get(X-1) + memoC.get(X-2)
+	memoC[X] = aux
+	return memo[X]
 
 N = int(input())
 
 for i in range(N):
-	calls = [0]
 	X = int(input())
-	a = fib(X, calls,)
-	print (f"{calls[0]} {a}")
-	print(memo)
+	calls = 0
+	aux = 0
+	'''if (X <= 1):
+		calls = 1
+	else:
+		calls = 2 ** (X-1) '''
+	a = fib(X)
+	print ('fib ('+str(X)+') = ',calls,'calls =', a)
+	print(memoC)
+	#print(memo)
 	
